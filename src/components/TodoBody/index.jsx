cf0e8c6a3870e46a5e652102cbcda74186da9a1f
MaxSavchuck <avatar2945@gmail.com>
2021-09-24 12:00:14 +0300

    create sample

---
+++ src/components/TodoBody/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TodoBody () {
+  return <div></div>;
+}
+
+export default TodoBody;
---
+++ src/components/TodoInputForm/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TodoInputForm () {
+  return <div></div>;
+}
+
+export default TodoInputForm;
